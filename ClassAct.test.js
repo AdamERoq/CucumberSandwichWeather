b832093161726895e7deca52adf2dff48661285b
Adam Eckley <adam.eckley@roq.co.uk>
2023-02-15 10:49:28 +0000

    Added a Navigation test

--- ClassAct.test.js
+++ ClassAct.test.js
@@ -22,8 +22,25 @@ test("Navigate to Window cleaning", async () => {
     await browser.waitForElementByCss(':not(h3.main-title__secondary)');
     const heading = await browser.getElementByCss('h1.main-title__primary');
     const elementHeading = await heading.getText();
-    expect(elementHeading).toBe('Window Cleaning and Reach & Wash');
-    
+    expect(elementHeading).toBe('Window Cleaning and Reach & Wash');  
+})
+
+test("Navigate to Upholstery and Steam Deep Clean", async () => {
+    const element = await browser.getElement('menu-item-5329');
+    await element.click();
+    await browser.waitForElementByCss('h3.main-title__secondary');
+    const heading = await browser.getElementByCss('h1.main-title__primary');
+    const elementHeading = await heading.getText();
+    expect(elementHeading).toBe('Upholstery and Steam Deep Clean');
+})
+
+test("Navigate to contact us", async () => {
+    const element = await browser.getElement('menu-item-2269');
+    await element.click();
+    await browser.waitForElementByCss('h3.main-title__secondary'); 
+    const heading = await  browser.getElementByCss('h1.main-title__primary');
+    const text = await heading.getText();
+    expect(text).toBe('Contact Us');
 })
 
 test("Initially has a navigation bar with general maintenance", async () => {
@@ -31,6 +48,15 @@ test("Initially has a navigation bar with general maintenance", async () => {
     const tagName = await element.getText();
     expect(tagName).toBe('GENERAL MAINTENANCE');
 });
+
+test("Navigate to a new part of the webite using URL", async () => {
+    const element = browser.browserNavigate('https://www.classact.company/our-services/pool-maintenance/');
+    await element;
+    await browser.waitForElementByCss(':not(h3.main-title__secondary)');
+    const heading = await  browser.getElementByCss('h1.main-title__primary');
+    const text = await heading.getText();
+    expect(text).toBe('Pool Maintenance');
+})
 
 test("Initially has a navigation bar with upholstery", async () => {
     const element = await browser.getElement("menu-item-5329");
